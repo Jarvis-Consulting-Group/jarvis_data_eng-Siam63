3dab09b2317aadda801e38b5b4812a393f2f937c
Siam63 <siam_1000@hotmail.com>
2023-04-11 11:46:06 -0400

    hostname bug fixed, missing variable

--- linux_sql/scripts/host_usage.sh
+++ linux_sql/scripts/host_usage.sh
@@ -14,25 +14,19 @@ fi
 
 #Save machine statistics in MB and current machine hostname to variables
 vmstat_mb=$(vmstat --unit M)
+hostname=$(hostname -f)
 
 #Retrieve usage specification variables
 memory_free=$(echo "$vmstat_mb" | awk '{print $4}'| tail -n1 | xargs)
-
 cpu_idle=$(echo "$vmstat_mb" | tail -1 | awk -v col="15" '{print $col}' | xargs)
-
 cpu_kernel=$(echo "$vmstat_mb" | tail -1 | awk -v col="14" '{print $col}' | xargs)
-
 disk_io=$(vmstat -d | tail -1 | awk -v col="10" '{print $col}' | xargs)
-
 disk_available=$(df -BM / | tail -1 | awk -v col="4" '{print $col}' | xargs)
-
 timestamp=$(date '+%Y-%m-%d %H:%M:%S' | xargs)
 
 # Insert server usage data into host_usage table1
-insert_stmt="INSERT INTO host_usage(timestamp, host_id, memory_free, cpu_idel, cpu_kernel, disk_io, disk_available)
-                SELECT'$timestamp', id, '$memory_free', '$cpu_idle', '$cpu_kernel','$disk_io', '${disk_available%%M}'
-                FROM host_info
-                WHERE host_info.hostname='$psql_host'";
+insert_stmt="INSERT INTO host_usage (timestamp, host_id, memory_free, cpu_idel, cpu_kernel, disk_io, disk_available)
+               VALUES('$timestamp',(SELECT id FROM host_info WHERE hostname='$hostname'), $memory_free, $cpu_idle, $cpu_kernel, $disk_io, ${disk_available%%M})";
 
 #set up env var for pql cmd
 export PGPASSWORD=$psql_password
